60f027da8fd5113f75f06786539672cb5f6c3b07
Gary Blackwood <gary@garyblackwood.co.uk>
2025-12-30 13:22:49 +0000

    feat: textarea ui component

---
+++ src/components/ui/text-area.tsx
@@ -0,0 +1,31 @@
+import { UISize } from '@/components/ui';
+import { Textarea as BaseTextarea } from '@mantine/core';
+import { ChangeEvent, FocusEvent, KeyboardEvent } from 'react';
+
+export interface TextareaProps {
+  className?: string;
+  description?: string;
+  disabled?: boolean;
+  error?: string;
+  label?: string;
+  name: string;
+  onBlur?: (e: FocusEvent<HTMLTextAreaElement>) => void;
+  onChange?: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  onKeydown?: (e: KeyboardEvent<HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  radius?: UISize;
+  required?: boolean;
+  size?: UISize;
+  value: string;
+}
+
+// https://mantine.dev/core/textarea
+export function Textarea({ required, ...props }: TextareaProps) {
+  return (
+    <BaseTextarea
+      withAsterisk={required}
+      styles={{ wrapper: { height: '100%' }, input: { height: '100%' } }}
+      {...props}
+    />
+  );
+}
