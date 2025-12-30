27b5d744ffa9ef27e1046487b87069262f45f736
Gary Blackwood <gary@garyblackwood.co.uk>
2025-12-30 13:19:54 +0000

    feat: checkbox ui component

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,21 @@
+import { UISize } from '@/components/ui';
+import { Checkbox as BaseCheckbox, useMantineTheme } from '@mantine/core';
+import { ChangeEventHandler } from 'react';
+
+export interface CheckboxProps {
+  checked?: boolean;
+  className?: string;
+  defaultChecked?: boolean;
+  description?: string;
+  error?: string;
+  indeterminate?: boolean;
+  label?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  size?: UISize;
+}
+
+// https://mantine.dev/core/checkbox
+export function Checkbox(props: CheckboxProps) {
+  const theme = useMantineTheme();
+  return <BaseCheckbox color={theme.primaryColor} {...props} />;
+}
