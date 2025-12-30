10eee55a3ab91df1b5d6aac3e3db9ea29a7ef639
Gary Blackwood <gary@garyblackwood.co.uk>
2025-12-30 13:21:52 +0000

    feat: switch ui component

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,24 @@
+import { UISize } from '@/components/ui';
+import { Switch as BaseSwitch } from '@mantine/core';
+import { ChangeEventHandler, FocusEventHandler, ReactNode } from 'react';
+
+export interface SwitchProps {
+  checked: boolean;
+  description?: string;
+  disabled?: boolean;
+  error?: string;
+  label: string;
+  labelPosition?: 'left' | 'right';
+  name: string;
+  offLabel?: ReactNode;
+  onBlur: FocusEventHandler<HTMLInputElement>;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onLabel?: ReactNode;
+  radius?: UISize;
+  size?: UISize;
+}
+
+// https://mantine.dev/core/switch
+export function Switch(props: SwitchProps) {
+  return <BaseSwitch {...props} />;
+}
